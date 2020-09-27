6370d4faf7d78fbb07deaba7987e51fcd4a2cb19
simaung <ddhr00@gmail.com>
2020-09-27 20:59:59 +0700

    add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kasir App.
         </p>
+        <Button>Tombol Reactstrap</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
